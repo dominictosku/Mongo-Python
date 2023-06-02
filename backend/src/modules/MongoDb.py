e75e5a02421a90f19f6bed49ad6f9245ee9ca624
Dominic <dominic_tosku@kauz.ch>
2023-06-02 09:35:03 +0200

    feat: added get and put route

--- backend/src/modules/MongoDb.py
+++ backend/src/modules/MongoDb.py
@@ -9,17 +9,21 @@ class MongoDb():
 		client = MongoClient(connectionString)
 		self.client = client
 		self.database = client["JukeBox"]
+		self.collection = self.setCollection("songs")
+		
 
 	def findDocument(self, collection, document):
-		return self.database[collection].find_one(
+		return self.collection.find_one(
         {"_id": document.inserted_id}
     	)
 
 	def InsertToDB(self, collection, document):
 		return self.database[collection].insert_one(document)
-
-	def UpdateDB(self, collection, document):
-		self.database[collection].update_one(document)
+	
+	def UpdateDB(self, id, document):
+		filter = {"_id": id}
+		newvalues = {"$set": document}
+		return self.collection.update_one(filter, newvalues)
 
 	def getDatabase(self):
 		dblist = self.client.list_database_names()
@@ -50,22 +54,19 @@ class MongoDb():
 			print('-' + collection)
 		return collections
 
-	def setCollection(self):
-		print('Select Collections:')
-		collectionName = input()
-		collection = self.database[collectionName]
+	def setCollection(self, collectionName):
+		self.collection = self.database[collectionName]
 		if collectionName not in self.database.list_collection_names():
-			self.RestartProgram()
-			return
+			return None
 		print('Db: ' + self.database.name)
 		print('Collection: ' + collectionName)
-		return collection
+		return self.collection
 
 	def getDocuments(self, collection):
 		print('Documents')
-		for document in collection.find():
+		for document in self.collection.find(limit=100):
 			print(document['_id'])
-		return collection.find()
+		return list(self.collection.find(limit=100))
 	
 	def getDocumentFromId(self, collection):
 		print('Select Document:')
@@ -73,7 +74,7 @@ class MongoDb():
 		documentDict = []
 		queryId = input()
 		query = { "_id": bson.ObjectId(queryId) }
-		document = collection.find_one(query)
+		document = self.collection.find_one(query)
 		if document is None:
 			self.RestartProgram()
 			return
