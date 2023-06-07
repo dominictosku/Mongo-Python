b0467f4fe1026fd6210f890d79b8f26f008823db
egomann88 <justin-urbanek@bluewin.ch>
2023-06-07 10:56:10 +0200

    feat: User list added

--- jukebox/src/router/index.js
+++ jukebox/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ListsView from "../views/ListsView.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -8,6 +9,11 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomeView,
+        },
+        {
+            path: "/lists",
+            name: "lists",
+            component: ListsView
         },
     ],
 });
---
+++ jukebox/src/views/ListsView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container mx-auto p-4">
+        <div class="sm:flex justify-start mb-4">
+            <button class="btn w-full my-4 sm:w-auto sm:my-0 sm:mx-4">
+                Playlist erstellen
+            </button>
+            <button class="btn w-full my-4 sm:w-auto sm:my-0 sm:mx-4">
+                Song hinzufügen
+            </button>
+            <input type="text" class="search w-full sm:w-auto sm:mx-4" placeholder="Song suchen...">
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<style scoped></style>
