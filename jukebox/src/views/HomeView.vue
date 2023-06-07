373ce28dc8dd24380da9493e88eb1124fb6cfeca
egomann88 <justin-urbanek@bluewin.ch>
2023-06-07 09:48:35 +0200

    feat: add and filter songs

--- jukebox/src/views/HomeView.vue
+++ jukebox/src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="container mx-auto p-4">
+        <div class="sm:flex justify-start mb-4">
+            <button @click="addSong()" class="btn w-full my-4 sm:w-auto sm:my-0 sm:mx-4">
+                Song hinzufügen
+            </button>
+            <input v-model="inputSearch" type="text" class="search w-full sm:w-auto sm:mx-4" placeholder="Song suchen...">
+        </div>
+        <!--  -->
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-            <div v-for="song in songs" :key="song.id" class="bg-white rounded-md shadow p-4">
+            <div v-for="song in filteredSongs" :key="song.id" class="bg-white rounded-md shadow p-4">
                 <div class="flex justify-between mb-2">
                     <div class="font-semibold">{{ song.name }}</div>
                     <div class="space-x-2">
@@ -29,8 +36,9 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
+const inputSearch = ref('');
 const songs = ref([
     {
         id: 1,
@@ -56,6 +64,10 @@ const songs = ref([
     },
 ])
 
+const filteredSongs = computed(() => {
+    return songs.value.filter(x => x.name.toLowerCase().includes(inputSearch.value.toLowerCase()))
+})
+
 /* functions */
 
 // return the duration in h or m format
@@ -65,6 +77,18 @@ function durationValue(duration) {
         let minutes = duration % 60;
         return hours + " h " + minutes + " min";
     } else return duration + " min";
+}
+
+// adds a song to songs array (HArdcoded)
+function addSong() {
+    let song = {
+        id: Math.random() * 4249,
+        name: 'new Song',
+        attributes: ['new Attribute'],
+        duration: 424,
+        rating: 2.8
+    }
+    songs.value.push(song);
 }
 
 function addToPlaylist(song) {
