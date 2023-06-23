b91a7becfb65d4d36cfd9f8f5a2ee4a5c07eb883
Dominic <dominic_tosku@kauz.ch>
2023-06-23 08:36:08 +0200

    feat: added playlist

--- backend/src/models.py
+++ backend/src/models.py
@@ -44,3 +44,32 @@ class SongUpdate(BaseModel):
                 "url": "public/songs/a-call-to-the-soul.mp3"
             }
         }
+
+class Playlist(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    name: str = Field(...)
+    song: list[Song] = Field(None)
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "name": "Test",
+                "songs": []
+            }
+        }
+
+class PlaylistUpdate(BaseModel):
+    name: Optional[str]
+    song: Optional[list]
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "name": "Test",
+                "songs": []
+            }
+        }
