d340e0236cf6b4c85332d2c1098b523bd348f18a
Dominic <dominic_tosku@kauz.ch>
2023-06-28 13:16:59 +0200

    upload file

--- backend/src/routes/files_routes.py
+++ backend/src/routes/files_routes.py
@@ -1,18 +1,10 @@
-from fastapi import APIRouter, Body, Request, Response, HTTPException, status
+from fastapi import APIRouter, Body, Request, Response, HTTPException, status, File, UploadFile
 from fastapi.encoders import jsonable_encoder
 from typing import List
 
 from src.models.File import File, FileUpdate
 router = APIRouter()
 
-@router.get("/{id}", response_description="get list of songs", status_code=status.HTTP_200_OK, response_model=File)
-def get_file(id: str, request: Request):
-    file = request.app.MongoDb.findFileById(id)
-    return file
-
-@router.post("/", response_description="Create a new song", status_code=status.HTTP_201_CREATED, response_model=File)
-def create_file(request: Request, file: File = Body(...)):
-    file = jsonable_encoder(file)
-    new_file = request.app.MongoDb.InsertFile(file)
-    created_file = request.app.MongoDb.findFileById(new_file)
-    return created_file 
+@router.post("/", response_description="upload file", status_code=status.HTTP_200_OK, response_model=str)
+def post_file(file: UploadFile):
+    return {"filename": file.filename}
--- jukebox/src/views/testView.vue
+++ jukebox/src/views/testView.vue
@@ -1,10 +1,49 @@
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
+const file = ref();
+function handleFileUpload(event) {
+    file.value = event.target.files[0];
+}
+const config = {
+    headers: {
+        'content-type': 'multipart/form-data',
+        'Accept': 'application/json'
+    }
+};
+
+function submitFile() {
+    let formData = new FormData();
+    formData.append('file', file.value);
+
+    axios({
+        url: 'http://localhost:5000/files',
+        method: "post",
+        data: formData,   // DATA
+        headers: config.headers
+    })
+
+    axios.post()
+        .then(function () {
+            console.log('SUCCESS!!');
+        })
+        .catch(function () {
+            console.log('FAILURE!!');
+        });
+}
 </script>
 
 <template>
-    <div>
-
+    <div class="container">
+        <div>
+            <h2>Single File</h2>
+            <hr />
+            <label>File
+                <input type="file" @change="handleFileUpload($event)" />
+            </label>
+            <br>
+            <button v-on:click="submitFile()">Submit</button>
+        </div>
     </div>
 </template>
 
