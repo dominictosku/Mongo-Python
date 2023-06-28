028f35cb90dbded4c6499b57e92515348f65913f
egomann88 <justin-urbanek@bluewin.ch>
2023-06-28 11:32:50 +0200

    feat: link to create song

--- jukebox/src/views/HomeView.vue
+++ jukebox/src/views/HomeView.vue
@@ -28,9 +28,11 @@ function addSong() {
 <template>
     <div class=" mx-auto p-4">
         <div class="sm:flex justify-start mb-4">
-            <button @click="addSong()" class="btn w-full my-4 sm:w-auto sm:my-0 sm:mx-4">
-                Song hinzufügen
-            </button>
+            <router-link :to="'/manage-song/' + 0">
+                <button @click="addSong()" class="btn w-full my-4 sm:w-auto sm:my-0 sm:mx-4">
+                    Song hinzufügen
+                </button>
+            </router-link>
             <input v-model="inputSearch" type="text" class="search w-full sm:w-auto sm:mx-4" placeholder="Song suchen...">
         </div>
         <!--  -->
